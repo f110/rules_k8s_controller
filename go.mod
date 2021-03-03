9bb3fde2c7d109e22e8767a2f5570fb01c30c8af
Fumihiro Ito <fmhrit@gmail.com>
2021-03-04 01:02:04 +0900

    Add update-kustomize-assets script
    This script will update assets.bzl.

---
+++ go.mod
@@ -0,0 +1,10 @@
+module go.f110.dev/rules_k8s_controller
+
+go 1.16
+
+require (
+	github.com/bazelbuild/buildtools v0.0.0-20210227132407-f2aed9ee205d
+	github.com/google/go-github/v33 v33.0.0
+	github.com/spf13/pflag v1.0.5
+	go.starlark.net v0.0.0-20210223155950-e043a3d3c984
+)
---
+++ hack/update-kustomize-assets/main.go
@@ -0,0 +1,213 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+	"reflect"
+	"sort"
+	"strings"
+
+	"github.com/bazelbuild/buildtools/build"
+	"github.com/google/go-github/v33/github"
+	"github.com/spf13/pflag"
+)
+
+const (
+	KustomizeRepositoryOwner = "kubernetes-sigs"
+	KustomizeRepositoryName  = "kustomize"
+)
+
+type release struct {
+	Version string
+	Assets  []*asset
+}
+
+type asset struct {
+	OS     string
+	URL    string
+	SHA256 string
+}
+
+func getRelease(ver string) (*release, error) {
+	gClient := github.NewClient(nil)
+	rel, _, err := gClient.Repositories.GetReleaseByTag(
+		context.TODO(),
+		KustomizeRepositoryOwner,
+		KustomizeRepositoryName,
+		fmt.Sprintf("kustomize/%s", ver),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	assets := make(map[string]*asset)
+	checksums := make(map[string]string)
+	for _, v := range rel.Assets {
+		if v.GetName() == "checksums.txt" {
+			checksums, err = getChecksum(context.TODO(), v.GetBrowserDownloadURL())
+			if err != nil {
+				return nil, err
+			}
+			continue
+		}
+		s := strings.Split(v.GetName(), "_")
+		if s[3] != "amd64.tar.gz" {
+			continue
+		}
+		assets[v.GetName()] = &asset{
+			OS:  s[2],
+			URL: v.GetBrowserDownloadURL(),
+		}
+	}
+	newRelease := &release{Version: ver, Assets: make([]*asset, 0)}
+	for _, v := range assets {
+		u, err := url.Parse(v.URL)
+		if err != nil {
+			return nil, err
+		}
+		filename := filepath.Base(u.Path)
+		if checksum, ok := checksums[filename]; !ok {
+			return nil, fmt.Errorf("unknown filename: %s", filename)
+		} else {
+			v.SHA256 = checksum
+		}
+
+		newRelease.Assets = append(newRelease.Assets, v)
+	}
+
+	return newRelease, nil
+}
+
+func getChecksum(ctx context.Context, url string) (map[string]string, error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	contents, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	sums := make(map[string]string)
+	s := bufio.NewScanner(bytes.NewReader(contents))
+	for s.Scan() {
+		line := s.Text()
+		s := strings.Split(line, "  ")
+		sums[s[1]] = s[0]
+	}
+
+	return sums, nil
+}
+
+func updateKustomizeAssets(args []string) error {
+	assetsFile := ""
+	version := ""
+	overwrite := false
+	fs := pflag.NewFlagSet("update-kustomize-assets", pflag.ContinueOnError)
+	fs.StringVar(&assetsFile, "assets-file", "", "File path of assets.bzl")
+	fs.StringVar(&version, "version", "", "Version of kustomize")
+	fs.BoolVar(&overwrite, "overwrite", false, "Overwrite")
+	if err := fs.Parse(args); err != nil {
+		return err
+	}
+
+	buf, err := os.ReadFile(assetsFile)
+	if err != nil {
+		return err
+	}
+	f, err := build.Parse(filepath.Base(assetsFile), buf)
+	if err != nil {
+		return err
+	}
+	if len(f.Stmt) != 1 {
+		return errors.New("the file has to include dict assign only")
+	}
+	a, ok := f.Stmt[0].(*build.AssignExpr)
+	if !ok {
+		return fmt.Errorf("statement is not assign: %s", reflect.TypeOf(f.Stmt[0]).String())
+	}
+	dict, ok := a.RHS.(*build.DictExpr)
+	if !ok {
+		return fmt.Errorf("RHS is not dict: %s", reflect.TypeOf(a.RHS).String())
+	}
+	exists := make(map[string]*build.KeyValueExpr)
+	for _, v := range dict.List {
+		key, ok := v.Key.(*build.StringExpr)
+		if !ok {
+			continue
+		}
+		exists[key.Value] = v
+	}
+	if _, ok := exists[version]; ok {
+		log.Printf("%s is already exists", version)
+		return nil
+	}
+
+	if version != "" {
+		rel, err := getRelease(version)
+		if err != nil {
+			return err
+		}
+		dict.List = append(dict.List, releaseToKeyValueExpr(rel))
+		sort.Slice(dict.List, func(i, j int) bool {
+			return dict.List[i].Key.(*build.StringExpr).Value < dict.List[j].Key.(*build.StringExpr).Value
+		})
+	}
+	out := build.FormatString(f)
+	fmt.Print(out)
+
+	if overwrite {
+		if err := os.WriteFile(assetsFile, []byte(out), 0644); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func releaseToKeyValueExpr(release *release) *build.KeyValueExpr {
+	sort.Slice(release.Assets, func(i, j int) bool {
+		return release.Assets[i].OS < release.Assets[j].OS
+	})
+
+	files := make([]*build.KeyValueExpr, 0)
+	for _, v := range release.Assets {
+		files = append(files, &build.KeyValueExpr{
+			Key: &build.StringExpr{Value: v.OS},
+			Value: &build.TupleExpr{
+				List: []build.Expr{
+					&build.StringExpr{Value: v.URL},
+					&build.StringExpr{Value: v.SHA256},
+				},
+			},
+		})
+	}
+	kv := &build.KeyValueExpr{
+		Key: &build.StringExpr{Value: release.Version},
+		Value: &build.DictExpr{
+			List: files,
+		},
+	}
+
+	return kv
+}
+
+func main() {
+	if err := updateKustomizeAssets(os.Args); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+}
