9eae1755cd513619a313798988f21099b65ff00b
Fumihiro Ito <fmhrit@gmail.com>
2021-03-06 03:05:04 +0900

    WIP

--- k8s/code-generator.bash
+++ k8s/code-generator.bash
@@ -17,29 +17,34 @@ SRC_DIRS=@@SRC_DIRS@@
 GO_ROOT=@@GO_ROOT@@
 RUNFILE_DIR=$(pwd)
 MODULE_NAME=@@MODULE_NAME@@
+HEADER_FILE=$RUNFILE_DIR/@@HEADER_FILE@@
 
 GEN="$RUNFILE_DIR/$BIN"
 GAZELLE="$RUNFILE_DIR/$GAZELLE_PATH"
 
-#rm -rf src
-#mkdir -p src
+rm -rf src
+mkdir -p src
 
-#if [ -n "$(ls vendor 2> /dev/null)" ]; then
-#    for f in vendor/*; do
-#        ln -sf $RUNFILE_DIR/$f $RUNFILE_DIR/src
-#    done
-#fi
+if [ -n "$(ls vendor 2> /dev/null)" ]; then
+    mkdir -p ${RUNFILE_DIR}/src/${MODULE_NAME}/vendor
+    for f in vendor/*; do
+        ln -sf $RUNFILE_DIR/$f $RUNFILE_DIR/src/${MODULE_NAME}/vendor/
+    done
+fi
 
-#for i in "${!SRC_PACKAGE_DIRS[@]}"; do
-#    mkdir -p src/$(dirname ${SRC_PACKAGE_DIRS[$i]})
-#    ln -sf $RUNFILE_DIR/${SRC_DIRS[$i]} src/${SRC_PACKAGE_DIRS[$i]}
-#done
+for i in "${!SRC_PACKAGE_DIRS[@]}"; do
+    mkdir -p src/$(dirname ${SRC_PACKAGE_DIRS[$i]})
+    ln -sf $RUNFILE_DIR/${SRC_DIRS[$i]} src/${SRC_PACKAGE_DIRS[$i]}
+done
 
 unset GO111MODULE
 export GOROOT=$RUNFILE_DIR/$GO_ROOT
+export GOPATH=${RUNFILE_DIR}
+
+cd src/${MODULE_NAME}
 echo "module ${MODULE_NAME}" > go.mod
 echo "go 1.16" >> go.mod
-"$GEN" "--output-base=$RUNFILE_DIR" "${ARGS[@]}"
+"$GEN" "--output-base=$RUNFILE_DIR" "--go-header-file=$HEADER_FILE" "${ARGS[@]}"
 
 cd "$BUILD_WORKSPACE_DIRECTORY"
 
